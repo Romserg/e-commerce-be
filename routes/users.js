0c6278d6e23491eba68c261bc938eee43a62cbc3
Romserg <serg.romankov@gmail.com>
2022-01-17 15:55:27 +0200

    Get User and List of Users excluding password

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,26 @@ import bcrypt from 'bcryptjs'
 const router = express.Router();
 
 router.get(`/`, async (req, res) =>{
-  const userList = await User.find();
+  const userList = await User.find().select('-passwordHash');
 
   if(!userList) {
     res.status(500).json({success: false})
   }
   res.send(userList);
 })
+
+router.get(`/:id`, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select('-passwordHash');
+
+    if (!user)
+      return res.status(400).json({ success: false, message: 'User not found' });
+
+    return res.status(200).send(user);
+  } catch (err) {
+    return res.status(500).json({ success: false, error: err });
+  }
+});
 
 router.post(`/`, async (req, res) => {
   let user = new User({
