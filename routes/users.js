6f9bd36da881ac2525c1992ac4835412faee1815
Romserg <serg.romankov@gmail.com>
2022-01-17 16:05:51 +0200

    Update User data with/without password

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import { User } from '../models/user.js';
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs';
 
 const router = express.Router();
 
-router.get(`/`, async (req, res) =>{
+router.get(`/`, async (req, res) => {
   const userList = await User.find().select('-passwordHash');
 
-  if(!userList) {
-    res.status(500).json({success: false})
+  if (!userList) {
+    res.status(500).json({ success: false });
   }
   res.send(userList);
-})
+});
 
 router.get(`/:id`, async (req, res) => {
   try {
@@ -45,6 +45,43 @@ router.post(`/`, async (req, res) => {
     return res.status(400).send('The user can\'t be created');
 
   return res.status(200).send(user);
+});
+
+router.put(`/:id`, async (req, res) => {
+  try {
+    const userExist = await User.findById(req.params.id);
+    let newPassword;
+
+    if (req.body.password) {
+      newPassword = bcrypt.hashSync(req.body.password, 10);
+    } else {
+      newPassword = userExist.passwordHash;
+    }
+
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: newPassword,
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+      },
+      { new: true },
+    );
+
+    if (!user)
+      return res.status(400).send('The user can\'t be updated');
+
+    return res.status(200).send(user);
+  } catch (err) {
+    return res.status(500).json({ success: false, error: err });
+  }
 });
 
 export { router };
